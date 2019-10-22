ae556fce743c39a10b9ef556c6a727a06661d8a3
martiriera <martiriera13@gmail.com>
2019-10-22 20:09:53 +0200

    Teoria

---
+++ terminalEditor/src/teoria/readLine.java
@@ -0,0 +1,8 @@
+
+while ((sym = read()) ! = '\r' ){ //'\r' and not '\n' because console is on raw mode and not cooked
+  switch(sym){
+  ...
+  }
+}
+
+//El codi de presentació ha d'estar a readLine (sense MVC), es simplifcara al fer servir MVC
